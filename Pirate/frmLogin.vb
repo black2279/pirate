1e37705b22c135339b0bd93a1947f87e34d59708
Cristian Andrighetto <cristian_andrighetto94@virgilio.it>
2019-01-08 18:34:28 +0100

    Added Proxy section

--- Pirate/frmLogin.vb
+++ Pirate/frmLogin.vb
@@ -14,6 +14,7 @@
     End Sub
 
     Private Sub frmLogin_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+        My.Settings.Save()
         If (Not Manager.Music.IsLoggedIn) Then
             Environment.Exit(0)
         End If
@@ -21,6 +22,31 @@
 
     Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         SetLoginButtonStatus()
+        cbxUseProxy.Checked = My.Settings.UseProxy
+        txtProxyHost.Text = My.Settings.ProxyHost
+        txtProxyPort.Text = My.Settings.ProxyPort
+
+        Dim newPosition As Point = pnlInfo.Location
+
+        If (cbxUseProxy.Checked = False) Then
+            lblProxyHost.Visible = False
+            txtProxyHost.Visible = False
+            lblProxyPort.Visible = False
+            txtProxyPort.Visible = False
+            btnUpdateProxy.Visible = False
+            newPosition.Y = cbxUseProxy.Location.Y + 20
+            Height = newPosition.Y + 80
+        Else
+            lblProxyHost.Visible = True
+            txtProxyHost.Visible = True
+            lblProxyPort.Visible = True
+            txtProxyPort.Visible = True
+            btnUpdateProxy.Visible = True
+            newPosition.Y = btnUpdateProxy.Location.Y + 30
+            Height = newPosition.Y + 80
+        End If
+
+        pnlInfo.Location = newPosition
     End Sub
 
     Private Sub txtUsername_TextChanged(sender As Object, e As EventArgs) Handles txtUsername.TextChanged
@@ -35,4 +61,39 @@
         btnLogin.Enabled = Not String.IsNullOrWhiteSpace(txtUsername.Text) AndAlso Not String.IsNullOrWhiteSpace(txtPassword.Text)
     End Sub
 
+    Private Sub cbxUseProxy_CheckedChanged(sender As Object, e As EventArgs) Handles cbxUseProxy.CheckedChanged
+        My.Settings.UseProxy = cbxUseProxy.Checked
+
+        Dim newPosition As Point = pnlInfo.Location
+
+        If (cbxUseProxy.Checked = False) Then
+            lblProxyHost.Visible = False
+            txtProxyHost.Visible = False
+            lblProxyPort.Visible = False
+            txtProxyPort.Visible = False
+            btnUpdateProxy.Visible = False
+            newPosition.Y = cbxUseProxy.Location.Y + 20
+            Height = newPosition.Y + 80
+        Else
+            lblProxyHost.Visible = True
+            txtProxyHost.Visible = True
+            lblProxyPort.Visible = True
+            txtProxyPort.Visible = True
+            btnUpdateProxy.Visible = True
+            newPosition.Y = btnUpdateProxy.Location.Y + 30
+            Height = newPosition.Y + 80
+        End If
+
+        pnlInfo.Location = newPosition
+    End Sub
+
+    Private Sub btnUpdateProxy_Click(sender As Object, e As EventArgs) Handles btnUpdateProxy.Click
+        Dim ProxyPort As UInt32 = 0
+        If (Int32.TryParse(txtProxyPort.Text, ProxyPort)) Then
+            My.Settings.ProxyPort = Trim(txtProxyPort.Text)
+            My.Settings.ProxyHost = Trim(txtProxyHost.Text)
+        Else
+            MessageBox.Show("Invalid Proxy Port", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
 End Class
